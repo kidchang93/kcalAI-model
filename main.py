bf3a664dc453ec4155dcad55f92b74cef34c9cb1
kidchang93 <lck930721@gmail.com>
2025-09-24 22:54:57 +0900

    github Actions 배포 테스트(디버깅용)

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import io
+import logging
 
 from PIL import Image
 from fastapi import FastAPI, UploadFile, File
 from starlette.responses import JSONResponse
 from transformers import pipeline
+
+from log_utils import setup_level_logger
+
+# 로거 생성
+info_logger = setup_level_logger(logging.INFO)
 
 app = FastAPI()
 # Hugging Face 이미지 분류 파이프라인(음식 특화 모델)
@@ -11,6 +17,7 @@ classifier = pipeline(
     "image-classification",
     model="nateraw/food",
 )
+
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
@@ -21,7 +28,8 @@ async def predict(file: UploadFile = File(...)):
 
         # 모델 예측
         results = classifier(image)
-
+        info_logger.info(f"{file} 정상 수집 완료")
         return JSONResponse(content={"predictions": results[:3]})
     except Exception as e:
+        info_logger.info(f"exception 발생{file}")
         return JSONResponse(content={"error": str(e)}, status_code=500)
