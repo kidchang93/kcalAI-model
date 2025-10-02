446d97c4e64cb3057820bd0c08e818639f606916
kidchang93 <lck930721@gmail.com>
2025-10-02 17:55:36 +0900

    직접 학습 시킨 AI 모델 적용

--- services/predict_service.py
+++ services/predict_service.py
@@ -3,14 +3,40 @@ import io
 
 from PIL import Image
 from transformers import pipeline
+from ultralytics.models import YOLO
 
-classifier = pipeline(
-    "image-classification",
-    model="nateraw/food",
-)
+# classifier = pipeline(
+#     "image-classification",
+#     model="nateraw/food",
+# )
+#
+# def predict_image(image_bytes: bytes):
+#     image = Image.open(io.BytesIO(image_bytes))
+#     results = classifier(image)
+#
+#     return [{"label": r["label"], "score": r["score"]} for r in results[:3]]
+
+model = YOLO("runs/classify/korean_food/weights/best.pt")
 
 def predict_image(image_bytes: bytes):
-    image = Image.open(io.BytesIO(image_bytes))
-    results = classifier(image)
+    # byte Image 를 다시 컨버팅한다. 다른 형식들로 인해 발생되는 오류 방지
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    result = model(image)
+    probs = result[0].probs
 
-    return [{"label": r["label"], "score": r["score"]} for r in results[:3]]
+    # 상위 3개 예측
+    top3_indices = probs.top5[:3]
+    top3_names = [result[0].names[int(i)] for i in top3_indices]
+    top3_confs = [float(probs.data[int(i)].item()) for i in top3_indices]
+
+    # # Top-1 인덱스
+    # top1_idx = probs.top1
+    #
+    # # Top-1 클래스명
+    # label = result[0].names[top1_idx]
+    #
+    # # Top-1 confidence
+    # confidence = float(probs.top1conf)
+    predictions = [{"label": n, "score": c} for n, c in zip(top3_names, top3_confs)]
+
+    return predictions
